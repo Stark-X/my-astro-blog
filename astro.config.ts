fc21813c164cab242c8cdfbacfa840dd79c92c43
Stark-X <xiaojiezhi2@gmail.com>
2023-11-30 18:22:32 +0800

    fix: use output=static for SSG

--- astro.config.ts
+++ astro.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
     },
   },
   scopedStyleStrategy: "where",
-  output: "server",
+  output: "static",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
