835bedcf5c56c0b241c67f2517d4400ba2aa0853
Stark-X <xiaojiezhi2@gmail.com>
2023-12-01 18:25:11 +0800

    feat: add PV for each post with vercel-kv
    * feat: use vercel kv for pv
    
    * fix: fix that deprecated usage in setPv
    
    * test: try output: server will fix setPv 404 or not
    
    * Revert "test: try output: server will fix setPv 404 or not"
    
    This reverts commit 7840f7039faa65d378ef47120b836524b626a7e2.
    
    * fix: correct setPv.ts to setPV.ts
    
    * fix: the PageView will not disappear when enter a new page now
    
    * fix: delete confused setPv and setPV
    
    * fix: readd setPV
    
    * feat: make the pv in the same line with pubDatetime
    
    * test: add console.log for debug
    
    * fix: prevent prerender the setPV api

---
+++ src/pages/api/setPV.ts
@@ -0,0 +1,45 @@
+import type { APIRoute } from "astro";
+import { createClient } from "@vercel/kv";
+
+const KV_REST_API_URL = import.meta.env.KV_REST_API_URL;
+const KV_REST_API_TOKEN = import.meta.env.KV_REST_API_TOKEN;
+const isDev = import.meta.env.DEV;
+
+// prevent prerender the api return
+export const prerender = false;
+export const GET: APIRoute = async ctx => {
+  const v = ctx.url.searchParams.get("v") || "404";
+  console.log("update pv for", v);
+
+  if (isDev) {
+    return new Response(
+      JSON.stringify({
+        state: "ok",
+        message: "999",
+      })
+    );
+  }
+  try {
+    if (!KV_REST_API_URL || !KV_REST_API_TOKEN) {
+      throw new Error("missing env");
+    }
+    const v = ctx.url.searchParams.get("v") || "404";
+    const client = createClient({
+      url: KV_REST_API_URL,
+      token: KV_REST_API_TOKEN,
+    });
+
+    const number = await client.incr(`PV_${v}`);
+    return new Response(
+      JSON.stringify({
+        state: "ok",
+        message: number,
+      })
+    );
+  } catch (error) {
+    return new Response(null, {
+      status: 500,
+      statusText: "Internal Server Error",
+    });
+  }
+};
