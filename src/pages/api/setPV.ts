51f30eeddfd2232794b76b0c5612d6add2f96f02
Stark-X <xiaojiezhi2@gmail.com>
2023-12-01 16:23:30 +0800

    fix: correct setPv.ts to setPV.ts

---
+++ src/pages/api/setPV.ts
@@ -0,0 +1,31 @@
+import type { APIRoute } from "astro";
+import { createClient } from "@vercel/kv";
+
+const KV_REST_API_URL = import.meta.env.KV_REST_API_URL;
+const KV_REST_API_TOKEN = import.meta.env.KV_REST_API_TOKEN;
+
+export const GET: APIRoute = async ctx => {
+  try {
+    if (!KV_REST_API_URL || !KV_REST_API_TOKEN) {
+      throw new Error("missing env");
+    }
+    const v = ctx.url.searchParams.get("v") || "404";
+    const client = createClient({
+      url: KV_REST_API_URL,
+      token: KV_REST_API_TOKEN,
+    });
+
+    const number = await client.incr(`PV_${v}`);
+    return new Response(
+      JSON.stringify({
+        state: "ok",
+        message: number,
+      })
+    );
+  } catch (error) {
+    return new Response(null, {
+      status: 500,
+      statusText: "Internal Server Error",
+    });
+  }
+};
